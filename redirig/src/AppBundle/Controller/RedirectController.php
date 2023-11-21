24d4ccc607924b54c23f381d8ea9c2917f25435c
Daniel Tejerizo <daniel.tejmor@educa.jcyl.es>
2023-11-21 17:29:31 +0100

    .

--- redirig/src/AppBundle/Controller/RedirectController.php
+++ redirig/src/AppBundle/Controller/RedirectController.php
@@ -2,10 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class RedirectController extends Controller{
 
-    public function internalRedirectPermanentAction(){
-        return $this->redirectToRoute('redirect_index', array(), 301);
+    public function internalRedirectAction(){
+        return $this->redirect("http://google.es");
     }
 
 }
