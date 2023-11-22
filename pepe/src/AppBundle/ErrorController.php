83603326f80ab1637b05beb2578288363713fe5e
Diego de los Santos <diego.sansan.18@educa.jcyl.es>
2023-11-22 17:46:17 +0100

    .

--- pepe/src/AppBundle/ErrorController.php
+++ pepe/src/AppBundle/ErrorController.php
@@ -7,17 +7,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ErrorController extends Controller
 {
 
-    public function getAction($id){
+    public function getAction($id,SessionInterface $session){
         if($id == null || $id != 1){
             throw $this->createNotFoundException('Este es el error que saca php cuando no le pasas bien el parametro'); // Es el nombre de la excepción
         }
         else {
-            return new Response('Register: '.$id);
-        }
+            
+            $session->set('username',$id);
+
+            $idsesion = $session->get('username');
+            $session->remove('username'); 
+            $idsesion = $session->get('username');
+            return new Response('Register: '.$id.', el username de sesion es: '.$idsesion);
+        }   
     }
  
     public function createAction($username){
